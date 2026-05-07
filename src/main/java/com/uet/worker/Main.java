d9dee270042442159cb154a028f9012a3b48b0a7
Nguyen An Thai <25020386@gmail.com>
2026-05-08 05:38:06 +0700

    Chạy test 1

--- src/main/java/com/uet/worker/Main.java
+++ src/main/java/com/uet/worker/Main.java
@@ -7,7 +7,9 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        //Bắt đầu chương trình, chạy test 1
         logger.info("Chương trình bắt đầu.");
+        //Giữ nguyên như cũ
 
         Worker worker = new Worker();
         Thread thread = new Thread(worker, "Worker-Thread");
